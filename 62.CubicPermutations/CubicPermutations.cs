7f2ee38399ca30a03b32429b67244bf15d7ef2d0
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-11-04 16:01:50 +0200

    Problem 62;

---
+++ 62.CubicPermutations/CubicPermutations.cs
@@ -0,0 +1,62 @@
+﻿namespace CubicPermutations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class CubicPermutations
+    {
+        private static BigInteger GetCube(BigInteger number)
+        {
+            return number * number * number;
+        }
+
+        private static string GetCubeHash(BigInteger number)
+        {
+            var numberAsString = number.ToString();
+            return String.Concat(numberAsString.OrderBy(p => p));
+        }
+
+        public static void Main()
+        {
+            var dtNow = DateTime.Now;
+            var smallestCubes = new Dictionary<string, int>();
+            var counts = new Dictionary<string, int>();
+            var results = new List<int>();
+            int iterator = 1;
+
+            while (true)
+            {
+                var hash = GetCubeHash(GetCube(iterator));
+                if (results.Count != 0)
+                {
+                    if (GetCube(iterator).ToString().Count() > GetCube(results.Last()).ToString().Count())
+                    {
+                        break;
+                    }
+                }
+                if (!smallestCubes.ContainsKey(hash))
+                {
+                    smallestCubes.Add(hash, iterator);
+                    counts.Add(hash, 1);
+                }
+                else
+                {
+                    counts[hash]++;
+                    if (counts[hash] == 5)
+                    {
+                        results.Add(smallestCubes[hash]);
+                    }
+                }
+                iterator++;
+            }
+
+            
+            var result = results.Min();
+            Console.WriteLine("The number is: {0}", result);
+            Console.WriteLine(DateTime.Now - dtNow);
+            Console.WriteLine();
+        }
+    }
+}
